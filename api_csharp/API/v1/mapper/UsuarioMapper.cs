89e238a8c3023f6828713dcdcad72decbc919e41
Geverson <geversonjosedesouza@gmail.com>
2022-06-20 23:59:17 -0300

    Classes de representação de retorno da API

---
+++ api_csharp/API/v1/mapper/UsuarioMapper.cs
@@ -0,0 +1,23 @@
+﻿using api_csharp.API.v1.model;
+using AutoMapper;
+using domain.model;
+
+namespace api_csharp.API.v1.mapper
+{
+    public class UsuarioMapper : Profile
+    {
+        public UsuarioResponse ToResponse(Usuario usuario)
+        {
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Usuario, UsuarioResponse>());
+            var mapper = new Mapper(config);
+            return mapper.Map<UsuarioResponse>(usuario);
+        }
+
+        public List<UsuarioResponse> ToListResponse(List<Usuario> usuarios)
+        {
+            var usuarioResponses = new List<UsuarioResponse>();
+            usuarios.ForEach(usuario => usuarioResponses.Add(ToResponse(usuario)));
+            return usuarioResponses;
+        }
+    }
+}
---
+++ api_csharp/API/v1/model/UsuarioResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace api_csharp.API.v1.model
+{
+    public class UsuarioResponse
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
